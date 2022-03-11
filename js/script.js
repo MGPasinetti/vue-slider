88865da260db4efc828bc29c0ed6f3309bc4a2b0
MGPasinetti <mg.pasinetti@gmail.com>
2022-03-11 15:07:23 +0100

    arrSlides done

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const app = Vue({
+    el: `#root`,
+    data: {
+        aciveIndex: 0,
+        arrSlides: [
+            {
+                title: 'Svezia',
+                img: 'img/01.jpg',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            },
+            {
+                title: 'Svizzera',
+                img: 'img/02.jpg',
+                text: 'Lorem ipsum',
+            },
+            {
+                title: 'Gran Bretagna',
+                img: 'img/03.jpg',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            },
+            {
+                title: 'Germania',
+                img: 'img/04.jpg',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            },
+            {
+                title: 'Paradise',
+                img: 'img/05.jpg',
+                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            },
+        ]
+    },
+});
